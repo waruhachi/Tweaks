f716cc3db25043f02b284472fc60d62869a0c9b3
waru <156133757+waruhachi@users.noreply.github.com>
2025-03-04 21:58:39 +0000

    [chore] renamed Crystal to Crystal-Logos and removed Crystal-ObjC

--- Crystal-ObjC/Preferences/NotificationKeys.h
+++
@@ -1,8 +0,0 @@
-//
-//  NotificationKeys.h
-//  Crystal
-//
-//  Created by Alexandra Aurora Göttlicher
-//
-
-static NSString* const kNotificationKeyPreferencesReload = @"codes.aurora.crystal.preferences.reload";
