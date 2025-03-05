d554a44fd070fcb6ae3a21e61ec76424c0b82781
waruhachi <156133757+waruhachi@users.noreply.github.com>
2025-03-05 00:42:12 -0600

    [chore] removed Copypasta

--- Copypasta/Tweak/CPAItem.h
+++
@@ -1,8 +0,0 @@
-#import <Foundation/Foundation.h>
-
-@interface CPAItem : NSObject
-@property(nonatomic, retain)NSString* title;
-@property(nonatomic, retain)NSString* bundleId;
-@property(nonatomic, retain)NSString* content;
-+ (CPAItem *)itemWithContent:(NSString *)content title:(NSString *)title bundleId:(NSString *)bundleId;
-@end
--- Copypasta/Tweak/CPALabel.m
+++
@@ -1,9 +0,0 @@
-#import "CPALabel.h"
-
-@implementation CPALabel
-
-- (void)drawTextInRect:(CGRect)rect {
-    [super drawTextInRect:UIEdgeInsetsInsetRect(rect, self.layoutMargins)];
-}
-
-@end
