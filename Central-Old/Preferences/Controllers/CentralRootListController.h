7bad164a5cadc3c95a007ad3461c49b88ead4810
waruhachi <156133757+waruhachi@users.noreply.github.com>
2025-03-05 00:55:08 -0600

    [chore] removed Astolfo-Old, Central-Old, Heartlines-Old, and VerticalBatteryDrain-Old

--- Central-Old/Preferences/Controllers/CentralRootListController.h
+++
@@ -1,4 +0,0 @@
-#import <Preferences/PSListController.h>
-
-@interface CentralRootListController : PSListController
-@end
--- VerticalBatteryDrain-Old/Sources/VerticalBatteryDrainC/include/Tweak.h
+++
@@ -1,8 +0,0 @@
-#import <UIKit/UIKit.h>
-
-@interface _UIBatteryView : UIView
-@property (nonatomic, strong, readwrite) CALayer *fillLayer;
-@end
-@interface CALayer ()
-- (void)setFrame:(CGRect)frame;
-@end
